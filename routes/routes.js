8bc59a1c72cb80a106daf685d4e59b06d54da988
Reggie P <reginaldpitts1@gmail.com>
2022-11-29 19:17:30 -0600

    editing routes exports

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,14 @@
 // this file will be for redirecting routes
+
+// declare variables as constants
+const fs = require('fs');
+const path = require('path');
+
+// module.exports exports a particular function
+module.exports = app => {
+
+    fs.readFile("db/db.json","utf8", (err, data) => {
+
+        if (err) throw err;
+
+        const notes = JSON.parse(data);
